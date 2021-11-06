ed872b15e4ad1417392e96738c2811ada04c3577
Apivit Denpruktham <apivit37463@gmail.com>
2021-11-06 16:53:35 +0700

    dsfgfg

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+import { CardList } from "./component/card-list/card-list.component";
+import { SearchBox } from "./component/search=box/search-box.component";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+    // .then((monster) => {
+    //   console.log(this.state.monsters);
+    // });
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1> Monters Rolodex </h1>
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
